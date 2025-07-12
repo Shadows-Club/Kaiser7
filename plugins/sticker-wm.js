66878d95809e4e9a91409c7180e218d745c5d278
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-07-12 07:15:44 -0500

    Actualizar sticker-wm.js

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -1,23 +1,36 @@
+
 import { addExif } from '../lib/sticker.js'
+
 let handler = async (m, { conn, text }) => {
-  if (!m.quoted) return m.reply(`*${emojis} Responda al sticker para poder cambiar los parámetros del sticker.*`)
+
+  if (!m.quoted) return conn.reply(m.chat, `*${emojis} Responda al sticker para cambiar los parámetros.*`, m, rcanal)
+
   let stiker = false
   try {
+    // Se separa el texto para obtener el nombre del paquete y el autor
     let [packname, ...author] = text.split('|')
     author = (author || []).join('|')
+
+    // Se valida si el archivo es un sticker (webp)
     let mime = m.quoted.mimetype || ''
-    if (!/webp/.test(mime)) return m.reply(`*${emojis} Responde al sticker.*`)
+    if (!/webp/.test(mime)) return conn.reply(m.chat, '*⚠️ El archivo adjunto no es un sticker, por favor responde a un sticker.*', m)
+
+    // Descarga el archivo citado
     let img = await m.quoted.download()
-    if (!img) return m.reply(`*${emojis} Por favor, responda al sticker.*`)
+    if (!img) return conn.reply(m.chat, `*${xsticker} Responde al sticker.*`, m)
+
+    // Se añade el EXIF al sticker
     stiker = await addExif(img, packname || '', author || '')
   } catch (e) {
     console.error(e)
     if (Buffer.isBuffer(e)) stiker = e
   } finally {
-    if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m)
-    else return m.reply(`*${emojis} Responda al sticker.*`)
+    // Si se ha creado el sticker, lo envía
+    if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, rcanal)
+    else return conn.reply(m.chat, `*${xsticker} Responde al sticker.*`, m)
   }
 }
+
 handler.help = ['wm']
 handler.tags = ['sticker']
 handler.command = ['take', 'wm']
