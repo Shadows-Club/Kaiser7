f5ea9ccbe0bd09d9a2a68905a5ebd68feb4cedd4
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-07-12 11:10:39 -0500

    Actualizar info-comprar_bot.js

--- plugins/info-comprar_bot.js
+++
@@ -1,8 +0,0 @@
-const handler = async (m, { conn }) => {
-
-  conn.sendMessage(m.chat, {
-text: `https://chat.whatsapp.com/DXA0VmYf3p9GDMjJUz8Mik`,
-}, { quoted: fkontak });
-};
-handler.command = ['comprar'];
-export default handler;
