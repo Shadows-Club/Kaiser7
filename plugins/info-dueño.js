fef90deafe122b4757d8d23c58795ed632a76fb2
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-07-12 11:16:18 -0500

    Actualizar info-dueño.js

--- plugins/info-dueño.js
+++ plugins/info-dueño.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn }) => {
-  await m.react('🕷️');
+  await m.react(emojis);
 
   // Crear contacto VCARD
   let list = [{
-    displayName: "Ignacio",
-    vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:Shinazugawa\nitem1.TEL;waid=56964656014:56964656014\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET:team.sunflare@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://www.instagram.com/nee\nitem3.X-ABLabel:Instagram\nitem4.ADR:;;Chile 🇨🇱;;;;\nitem4.X-ABLabel:País\nEND:VCARD`
+    displayName: "Maricielo",
+    vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:Morchi\nitem1.TEL;waid=51906168999:51906168999\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET:team.sunflare@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://www.instagram.com/shadows-club\nitem3.X-ABLabel:Instagram\nitem4.ADR:;;Perú 🇵🇪;;;;\nitem4.X-ABLabel:País\nEND:VCARD`
   }];
 
   await conn.sendMessage(m.chat, {
