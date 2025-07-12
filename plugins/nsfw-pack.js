b0a395245a77bf45fbaa6c5d72db882ba2852774
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-07-12 11:27:08 -0500

    Actualizar nsfw-pack.js

--- plugins/nsfw-pack.js
+++ plugins/nsfw-pack.js
@@ -1,25 +1,3 @@
-/*import fetch from 'node-fetch';
-
-import fetch from 'node-fetch';
-
-let handler = async(m, { conn, text, usedPrefix, command }) => {
-
-m.react('🕑');
-
-let txt = 'Pack🔥🔥🔥\n> Pon De Nuevo .pack para mirar el siguiente ✨';
-
-let img = 'https://delirius-apiofc.vercel.app/nsfw/girls';
-
-m.react('✅');
-conn.sendMessage(m.chat, { image: { url: img }, caption: txt }, { quoted: fkontak });
-}
-
-handler.command = ['pack'];
-
-export default handler;
-
-
-import fetch from 'node-fetch';*/
 
 let handler = async (m, { conn, usedPrefix, command }) => {
 
