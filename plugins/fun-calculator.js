11b6fd207d9293e69d2f1d2ef8b59cb254a5b19f
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-07-12 09:50:44 -0500

    Actualizar fun-calculator.js

--- plugins/fun-calculator.js
+++ plugins/fun-calculator.js
@@ -1,5 +1,5 @@
 const handler = async (m, { conn, command, text }) => {
-  if (!text) return conn.reply(m.chat, `*🐈  Menciona al usuario.*`, m);
+  if (!text) return conn.reply(m.chat, `*${emojis}  Menciona al usuario para calcular rl porcentaje.*`, m, rcanal);
 
   const percentages = Math.floor(Math.random() * 501);
   const emojis = {
