f60e485bdbcc8ca186841472e4e7545d7b724767
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-07-17 13:41:05 -0500

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -512,16 +512,16 @@ global.dfail = (type, m, conn) => {
 let user2 = m.pushName || 'Anónimo'
 
 const msg = {
-rowner: '*🐈 Esta función solo puede ser usada por el actual Owner.*', 
-owner: '*🐈 Esta función solo puede ser usada por mi Desarrollador.*', 
-mods: '*🐈 Esta función solo puede ser usada los moderadores del bot.*', 
-premium: '*🐈 Esta función solo es para usuarios Premium.*', 
-group: '*🐈 Esta funcion solo puede ser ejecutada en Grupos.*', 
-private: '*🐈 Esta función solo puede ser ejecutada en chat privado.*', 
-admin: '*🐈 Este comando solo puede ser usado por Admins.*', 
-botAdmin: '*🐈 Para usar esta función debo ser Admin.*',
-unreg: `*🐈 \`Morchi\` te avisa que no te encuentras registrado para usar esta función en el bot.*`,
-restrict: '*🐈 Esta característica esta desactivada.*'
+rowner: `*${emoji} Esta función solo puede ser usada por el actual Owner.*`, 
+owner: `*${emoji} Esta función solo puede ser usada por mi Desarrollador.*`, 
+mods: `*${emoji} Esta función solo puede ser usada los moderadores del bot.*`, 
+premium: `*${emoji} Esta función solo es para usuarios Premium.*`, 
+group: `*${emoji} Esta funcion solo puede ser ejecutada en Grupos.*`, 
+private: `*${emoji} Esta función solo puede ser ejecutada en chat privado.*`, 
+admin: `*${emoji} Este comando solo puede ser usado por Admins.*`, 
+botAdmin: `*${emoji} Para usar esta función debo ser Admin.*`,
+unreg: `*${emoji} \`Morchi\` te avisa que no te encuentras registrado para usar esta función en el bot.*`,
+restrict: `*${emoji} Esta característica esta desactivada.*`
 }[type];
 if (msg) return conn.reply(m.chat, msg, m, rcanal).then(_ => m.react('✖️'))}
 
